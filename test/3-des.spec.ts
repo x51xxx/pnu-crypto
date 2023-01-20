12a924ac73af4fef19567bc20ce1244f8c1cb75c
Taras Trishchuk <ttrishchuk@softjourn.com>
2023-01-20 23:34:30 +0200

    fix: fixed des e2e test

--- test/3-des.spec.ts
+++ test/3-des.spec.ts
@@ -6,8 +6,8 @@ const desLib = require('../src/lib/des');
 
 describe('DES', function () {
     it('should encrypt/decryption', function () {
-        const input = 'Lorem Ipsum is simply dummy text of the printing and typesetting'
-        assert.deepEqual(input.length, 64);
+        const input = 'Lorem Ip' // 64 bit
+        assert.deepEqual(input.length, 8);
 
         const key = [0x13, 0x34, 0x57, 0x79, 0x9B, 0xBC, 0xDF, 0xF1];
         const enc = desLib.DES.create({
@@ -17,8 +17,6 @@ describe('DES', function () {
 
         const out = new Uint8Array([...enc.update(str_to_bytes(input)), ...enc.final()]);
 
-        console.log(out)
-
         const dec = desLib.DES.create({
             type: 'decrypt',
             key,
@@ -27,9 +25,8 @@ describe('DES', function () {
 
         const decoder = new TextDecoder();
 
-        console.log(decoder.decode(new Uint8Array(decryptText)))
+        console.log('decode text:', decoder.decode(new Uint8Array(decryptText)))
 
         assert.equal(input, bytes_to_str(new Uint8Array(decryptText)));
-
     })
 });
